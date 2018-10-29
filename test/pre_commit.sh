00e32746800031c2d8b17cea5f867b87cfaf0dad
Jiahong He <jh3863@columbia.edu>
2018-10-29 11:36:54 -0400

    iteration one

--- test/pre_commit.sh
+++ test/pre_commit.sh
@@ -1,4 +1,6 @@
 echo "Running static code analysis"
-find . -iname "*.py" | xargs pylint 
+cd ../
+find . -iname "*.py" | xargs pylint >> test/reports/styleCheckReport.txt
 pip3 install -r requirments.txt
-bash test/test_main.sh
+bash test/test_main.sh 
+cd test
--- test/test_main.sh
+++ test/test_main.sh
@@ -1,8 +1,8 @@
 cd src/server/backendServer
 echo "testing server"
-python3 manage.py test
+python3 manage.py test >> ../../../test/reports/testingReport.txt
 cd ../../../
 cd test
 echo "testing FaceRecog"
-python3 -m unittest getAllUsers_test.py
+python3 -m unittest getAllUsers_test.py >> reports/testingReport.txt
 cd ..
