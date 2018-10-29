8c97c4e1362a8678aaa92adb2c081aefb06bf66c
Jiahong He <jh3863@columbia.edu>
2018-10-29 00:58:45 -0400

    iteration one almost done

---
+++ main.sh
@@ -0,0 +1,3 @@
+python3 src/server/backendServer/manage.py runserver
+echo "program running, use cmd+q to quit the face recognition module, and then ctl+c to stop the server"
+python3 src/FaceRecog/FC.py
