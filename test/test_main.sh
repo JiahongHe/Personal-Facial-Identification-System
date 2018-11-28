e1b1ad643197754296232611cc40bdf4e1cad855
Evan Ziebart <erziebart@gmail.com>
2018-11-27 20:30:13 -0500

    got coverage.py to work and added test coverage results

--- test/test_main.sh
+++ test/test_main.sh
@@ -1,8 +1,10 @@
 cd src/server/backendServer
 echo "testing server"
-python3 manage.py test >> ../../../test/reports/testingReport.txt
+coverage run --rcfile=../../../.coveragerc manage.py test >> ../../../test/reports/testingReport.txt
 cd ../../../
 cd test
 echo "testing FaceRecog"
-python3 -m unittest getAllUsers_test.py >> reports/testingReport.txt
+coverage run --rcfile=../.coveragerc -m unittest getAllUsers_test.py >> reports/testingReport.txt
 cd ..
+coverage combine src/server/backendServer/.coverage test/.coverage
+coverage report
