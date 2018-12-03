6c7ae61d210d40b4e8c90b0066dc41444aae4bc1
Jiahong He <jh3863@columbia.edu>
2018-12-03 14:27:53 -0500

    added coverag.py to master

--- src/backendServer/requestHandler/urls.py
+++ src/backendServer/requestHandler/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('recognitionRequest', views.recognitionRequestHandler, name='recognize'),
     path('requestInfo', views.requestInfo, name='test'),
     path('requestLoginInfo', views.requestLoginInfo, name='requestLoginInfo'),
     path('userUpdate', views.requestUpdateUserInfo, name='userUpdate'),
--- src/backendServer/requestHandler/views.py
+++ src/backendServer/requestHandler/views.py
@@ -8,26 +8,6 @@ fields = ['FirstName',
          'LastName',
          'Email',
          'passWord']
-currentUser = None
-
-def recognitionRequestHandler(request):
-
-    # handles the face recognition request, currently not in use now, maybe implemented in the future.
-
-    if request.method == 'GET':
-        image = request.FILES
-        return HttpResponse(image, content_type="image/jpeg")
-
-        '''
-        response = {}
-        picture = [2, 3, 4]
-        requestedUser = recognitionRequestObject(picture)
-        result = recognize(requestedUser)
-        response["userId"] = result.userId
-        response["song_file_path"] = result.favouriteSongPath
-        response["time_processed"] = result.time_recognized
-        return HttpResponse(json.dumps(response))
-        '''
 
 def requestInfo(request):
     # API that returns all the necessary user infomation for facial recognition
@@ -92,4 +72,3 @@ def requestUpdateUserInfo(request):
                 return HttpResponse("user not found")
         else:
             return HttpResponse("invalid information")
-
